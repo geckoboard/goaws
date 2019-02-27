fd7a7702bd137743976228c191e26e542cb99e6f
Jon Normington <jnormington@users.noreply.github.com>
2019-02-27 08:21:43 +0000

    Set message timestamps to epoch milliseconds
    As per the AWS SQS docs the SentTimestamp and ApproximateFirstReceiveTimestamp are epoch time in milliseconds not epoch
    
    https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_Message.html
    > ApproximateFirstReceiveTimestamp and SentTimestamp are each returned as an integer representing the epoch time in milliseconds.

--- app/gosqs/gosqs.go
+++ app/gosqs/gosqs.go
@@ -854,10 +854,10 @@ func getMessageResult(m *app.Message) *app.ResultMessage {
 	}
 
 	attrsMap := map[string]string{
-		"ApproximateFirstReceiveTimestamp": fmt.Sprintf("%d", m.ReceiptTime.Unix()),
+		"ApproximateFirstReceiveTimestamp": fmt.Sprintf("%d", m.ReceiptTime.UnixNano()/1e+6),
 		"SenderId":                         app.CurrentEnvironment.AccountID,
 		"ApproximateReceiveCount":          fmt.Sprintf("%d", m.NumberOfReceives+1),
-		"SentTimestamp":                    fmt.Sprintf("%d", time.Now().UTC().Unix()),
+		"SentTimestamp":                    fmt.Sprintf("%d", time.Now().UTC().UnixNano()/1e+6),
 	}
 
 	var attrs []*app.ResultAttribute
